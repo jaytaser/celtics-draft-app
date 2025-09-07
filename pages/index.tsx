32f63fcb996d99b87b9c544c6e77e773c3eace0c
jaytaser <j82554l@gmail.com>
2025-09-06 20:52:47 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
-
+ 
 /** ---------- Types ---------- */
 type Game = {
   id: number;
